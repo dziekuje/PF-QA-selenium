b2f6a5e2f88a32e65073bff3cb7d76f95b9af417
Andrew Mac <hub.mnk+git@gmail.com>
2020-10-15 10:42:39 +0200

    minor changes

--- utils/json_config_parse.py
+++ utils/json_config_parse.py
@@ -1,7 +1,6 @@
 import json
 
-
-FILE = './json_config_data.json'
+FILE = 'json_config_data.json'
 
 
 def get_json():
@@ -9,20 +8,14 @@ def get_json():
         x = json.load(json_file)
     return x
 
-
 def get_browser():
     x = get_json()
     return x['browser']
-
 
 def get_wait():
     x = get_json()
     return x['wait']
 
-
 def get_mainurl():
     x = get_json()
     return x['mainurl']
-
-
-print(get_browser())
